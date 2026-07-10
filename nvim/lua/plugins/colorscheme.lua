513ea8d3dfcbd1e506aa3dc24c6158637d0a2c33
Jason Korol <ejkorol@gmail.com>
2026-07-10 14:56:44 -0400

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -14,6 +14,13 @@ return {
         },
       })
       vim.cmd.colorscheme("vesper")
+
+      vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeTabActive", { fg = "#e0e0e0", bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeTabInactive", { fg = "#3c3c3c", bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeTabSeparatorActive", { fg = "NONE", bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeTabSeparatorInactive", { fg = "NONE", bg = "NONE" })
     end,
   },
 }
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -11,71 +11,53 @@ return {
     keys = {
       { "<leader>e", "<cmd>Neotree toggle current reveal_force_cwd left<cr>" },
     },
-    config = function()
-      require("neo-tree").setup({
-        enable_git_status = true,
-        open_files_do_not_replace_window = true,
-        window = {
-          position = "left",
-          width = 40,
-          mappings = {
-            ["<tab>"] = "next_source",
-            ["<S-tab>"] = "prev_source"
-          },
+    opts = {
+      enable_git_status = true,
+      open_files_do_not_replace_window = true,
+      window = {
+        position = "left",
+        width = 40,
+        mappings = {
+          ["<tab>"] = "next_source",
+          ["<S-tab>"] = "prev_source"
         },
-        filesystem = {
-          filtered_items = {
-            visible = true
-          }
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true
+        }
+      },
+      sources = {
+        "filesystem",
+        "buffers",
+        "git_status"
+      },
+      source_selector = {
+        winbar = true,
+        statusline = false,
+        show_scrolled_off_parent_node = false,
+        content_layout = "center",
+        tabs_min_width = nil,
+        tabs_max_width = nil,
+        padding = 0,
+        separator = { left = "", right = "" },
+        separator_active = nil,
+        show_separator_on_edge = false,
+        highlight_tab = "NeoTreeTabInactive",
+        highlight_tab_active = "NeoTreeTabActive",
+        highlight_background = "NeoTreeTabInactive",
+        highlight_separator = "NeoTreeTabSeparatorInactive",
+        highlight_separator_active = "NeoTreeTabSeparatorActive",
+      },
+      renderers = {
+        file = {
+          { "indent" },
+          { "icon" },
+          { "name",       use_git_status_colors = true },
+          { "git_status", highlight = "NeoTreeDimText" },
         },
-        sources = {
-          "filesystem",
-          "buffers",
-          "git_status"
-        },
-        source_selector = {
-          winbar = true,
-          statusline = false,
-          show_scrolled_off_parent_node = false,
-          content_layout = "center",
-          tabs_min_width = nil,
-          tabs_max_width = nil,
-          padding = 0,
-          separator = { left = "", right = "" },
-          separator_active = nil,
-          show_separator_on_edge = false,
-          highlight_tab = "NeoTreeTabInactive",
-          highlight_tab_active = "NeoTreeTabActive",
-          highlight_background = "NeoTreeTabInactive",
-          highlight_separator = "NeoTreeTabSeparatorInactive",
-          highlight_separator_active = "NeoTreeTabSeparatorActive",
-        },
-        renderers = {
-          file = {
-            { "indent" },
-            { "icon" },
-            { "name",       use_git_status_colors = true },
-            { "git_status", highlight = "NeoTreeDimText" },
-          },
-        },
-      })
-
-      -- winbar transparency, reapplied every time colorscheme reloads
-      local function set_neotree_winbar_hl()
-        vim.api.nvim_set_hl(0, "WinSeparator", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NeoTreeTabActive", { fg = "#e0e0e0", bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NeoTreeTabInactive", { fg = "#3c3c3c", bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NeoTreeTabSeparatorActive", { fg = "NONE", bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NeoTreeTabSeparatorInactive", { fg = "NONE", bg = "NONE" })
-      end
-
-      set_neotree_winbar_hl() -- apply once at startup, in case colorscheme already loaded
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        callback = set_neotree_winbar_hl,
-      })
-    end,
+      },
+    },
   },
   {
     "Crysthamus/nvim-file-operations",
