1ccf31a22c8d99e40fa4b3d368c08d88c39db3cd
Jason Korol <ejkorol@gmail.com>
2026-07-10 14:48:43 -0400

    update

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -11,52 +11,76 @@ return {
     keys = {
       { "<leader>e", "<cmd>Neotree toggle current reveal_force_cwd left<cr>" },
     },
-    opts = {
-      enable_git_status = true,
-      open_files_do_not_replace_window = true,
-      window = {
-        position = "left",
-        width = 40,
-        mappings = {
-          ["<tab>"] = "next_source",
-          ["<S-tab>"] = "prev_source"
+    config = function()
+      require("neo-tree").setup({
+        enable_git_status = true,
+        open_files_do_not_replace_window = true,
+        window = {
+          position = "left",
+          width = 40,
+          mappings = {
+            ["<tab>"] = "next_source",
+            ["<S-tab>"] = "prev_source"
+          },
         },
-      },
-      filesystem = {
-        filtered_items = {
-          visible = true
-        }
-      },
-      sources = {
-        "filesystem",
-        "buffers",
-        "git_status"
-      },
-      source_selector = {
-        winbar = true,
-        statusline = false,
-        show_scrolled_off_parent_node = false
-      },
-      renderers = {
-        file = {
-          { "indent" },
-          { "icon" },
-          { "name",       use_git_status_colors = true },
-          -- { "diagnostics" },
-          { "git_status", highlight = "NeoTreeDimText" },
-          -- { "file_size" },
-          -- { "last_modified" },
-          -- { "created" },
-          -- { "symlink_target", zindex = 10 },
+        filesystem = {
+          filtered_items = {
+            visible = true
+          }
         },
-      },
-    },
+        sources = {
+          "filesystem",
+          "buffers",
+          "git_status"
+        },
+        source_selector = {
+          winbar = true,
+          statusline = false,
+          show_scrolled_off_parent_node = false,
+          content_layout = "center",
+          tabs_min_width = nil,
+          tabs_max_width = nil,
+          padding = 0,
+          separator = { left = "", right = "" },
+          separator_active = nil,
+          show_separator_on_edge = false,
+          highlight_tab = "NeoTreeTabInactive",
+          highlight_tab_active = "NeoTreeTabActive",
+          highlight_background = "NeoTreeTabInactive",
+          highlight_separator = "NeoTreeTabSeparatorInactive",
+          highlight_separator_active = "NeoTreeTabSeparatorActive",
+        },
+        renderers = {
+          file = {
+            { "indent" },
+            { "icon" },
+            { "name",       use_git_status_colors = true },
+            { "git_status", highlight = "NeoTreeDimText" },
+          },
+        },
+      })
+
+      -- winbar transparency, reapplied every time colorscheme reloads
+      local function set_neotree_winbar_hl()
+        vim.api.nvim_set_hl(0, "WinSeparator", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NeoTreeTabActive", { fg = "#e0e0e0", bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NeoTreeTabInactive", { fg = "#3c3c3c", bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NeoTreeTabSeparatorActive", { fg = "NONE", bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NeoTreeTabSeparatorInactive", { fg = "NONE", bg = "NONE" })
+      end
+
+      set_neotree_winbar_hl() -- apply once at startup, in case colorscheme already loaded
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = set_neotree_winbar_hl,
+      })
+    end,
   },
   {
     "Crysthamus/nvim-file-operations",
-    -- branch = "compat" -- if you are on Neovim <= 0.10
     dependencies = {
-      "nvim-neo-tree/neo-tree.nvim", -- makes sure that this loads after Neo-tree.
+      "nvim-neo-tree/neo-tree.nvim",
     },
     config = function()
       require("nvim-file-operations").setup()
@@ -70,11 +94,8 @@ return {
         filter_rules = {
           include_current_win = false,
           autoselect_one = true,
-          -- filter using buffer options
           bo = {
-            -- if the file type is one of following, the window will be ignored
             filetype = { "neo-tree", "neo-tree-popup", "notify" },
-            -- if the buffer type is one of following, the window will be ignored
             buftype = { "terminal", "quickfix" },
           },
         },
