1a5106714322454b86a67b06d08812a358d23e98
Jason Korol <ejkorol@gmail.com>
2026-07-22 10:11:16 -0400

    update

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    dependencies = { "saghen/blink.cmp" },
+    lazy = false,
+    opts = {
+      markdown = {
+        enable = false
+      }
+    }
+  }
+}
